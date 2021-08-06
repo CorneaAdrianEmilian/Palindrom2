05c8455eb2c8ee840e71e0dd94ac4bf4f1ce78e2
Cornea Adrian Emilian <emilian.cornea@yahoo.com>
2021-08-06 18:05:33 +0300

    Descrierea functiilor

--- Palindrom2/CouldBePalindrom.h
+++ Palindrom2/CouldBePalindrom.h
@@ -1,4 +1,5 @@
 #pragma once
+//Functie ce testeaza daca un sir de caractere ar putea fi rearanjat intr-un palindrom
 #include <iostream>
 #include <string>
 #include <vector>
--- Palindrom2/Rearanjeaza.h
+++ Palindrom2/Rearanjeaza.h
@@ -1,4 +1,5 @@
 #pragma once
+//Functie ce rearanjeaza un sir de caractere intr-un palindrom
 #include <string>
 #include <vector>
 void rearanjeaza(std::string& sirul);
